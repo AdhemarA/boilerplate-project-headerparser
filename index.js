4e8401ef43b364be03f8b249cc0641dd63b5f676
AdhemarA <aahborgesnogueira@gmail.com>
2024-12-13 07:16:19 -0300

    configure express
    configure express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
+// require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -27,4 +27,13 @@ app.get('/api/hello', function (req, res) {
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+let headparsObj = {};
+app.enable("trust proxy");
+app.get("/api/whoami", (req, res) => {
+  headparsObj["ipaddress"] = req.ip;
+  headparsObj["language"] = req.get("Accept-Language");
+  headparsObj["software"] = req.get("User-Agent");
+  res.json( headparsObj);
 });
